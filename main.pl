8398c2aa2f523fa4e2a19c4f324b6581e6db6c87
Mateus Nóbrega <mateusn@outlook.com>
2018-05-03 17:17:08 -0300

    Added read file with text converstion to atoms and a test file

--- main.pl
+++ main.pl
@@ -4,3 +4,19 @@
 %% menu de instalacao de um pacote
 readInput :- write('Digite o nome do pacote: '), read(Package), 
 write(Package).
+
+
+main :-
+        open('db.txt', read, Str),
+        read_file(Str,Lines),
+        close(Str),
+        write('Linhas\n'), write(Lines), nl.
+    
+    read_file(Stream,[]) :-
+        at_end_of_stream(Stream).
+    
+    read_file(Stream,[X|L]) :-
+        \+ at_end_of_stream(Stream),
+        read_line_to_codes(Stream, Codes),
+        atom_chars(X, Codes),
+        read_file(Stream,L), !.
